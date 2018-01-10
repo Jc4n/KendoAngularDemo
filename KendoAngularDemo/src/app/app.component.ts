d651ebf86bd6e8e02899e78280c120a84e3c064d
Jc4n <neinjessifer@gmail.com>
2018-01-10 12:02:50 +0800

    add KendoAngular所需套件 初步套套看Kendo,可是樣式都好醜喔沒綁好的樣子

--- KendoAngularDemo/src/app/app.component.ts
+++ KendoAngularDemo/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../node_modules/@progress/kendo-theme-default/dist/all.css']
 })
 export class AppComponent {
   title = 'app';
+  public value: Date = new Date();
+  public listItems: Array<string> = ["Baseball", "Basketball", "Cricket", "Field Hockey", "Football", "Table Tennis", "Tennis", "Volleyball"];
+  public itemValue = ['Basketball', 'Cricket'];
+  public gridData: any[] = products;
+  onButtonClick(){
+    alert('search!');
+  }
 }
--- KendoAngularDemo/src/app/app.module.ts
+++ KendoAngularDemo/src/app/app.module.ts
@@ -5,12 +5,37 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+// Import the Animations module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Import the ButtonsModule
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { IntlModule } from '@progress/kendo-angular-intl';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { RippleModule } from '@progress/kendo-angular-ripple';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+     // Register the modules
+     BrowserAnimationsModule,
+     ButtonsModule,
+     InputsModule,
+     DateInputsModule,
+     IntlModule,
+     DropDownsModule,
+     RippleModule,
+     GridModule,
+     DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
